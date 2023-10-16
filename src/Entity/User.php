4871e36abbf965e1fba00ccd6140efe8b7721a3d
Ammar Khaoula <khaoulabha1991@gmail.com>
2023-10-16 15:24:33 +0200

    authentification

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -133,7 +133,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function isVerified(): bool
+    public function getIsVerified(): ?bool
     {
         return $this->isVerified;
     }
